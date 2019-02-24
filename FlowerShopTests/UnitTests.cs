9919e37ab55229a77879254d25b8a4f25aef64c2
nightstreak <prumo101@gmail.com>
2019-02-24 10:53:00 +0200

    Added a test case for SetDeliver.

--- FlowerShopTests/UnitTests.cs
+++ FlowerShopTests/UnitTests.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using FlowerShop;
+using NSubstitute;
 
 namespace Tests
 {
@@ -7,12 +9,21 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
+            //Act
+           
+            
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //Arrange
+            var order = Substitute.For<IOrderDAO>();
+            var client = Substitute.For<IClient>();
+            Order batch = Substitute.For<Order>(order,client, false);
+            //Act and Assert
+            batch.Received().Deliver();
+            
         }
     }
 }
